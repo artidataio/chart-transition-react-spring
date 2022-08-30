91d6de2179cfbb2b12ba0c7d417a49d21e2b7ccd
Imaduddin Haetami <khattami240193@gmail.com>
2022-08-30 11:10:14 +0700

    Add demo4 which utilize svg

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Demo1 from "./Demo1";
 import Demo2 from "./Demo2";
 import Demo3 from "./Demo3";
+import Demo4 from "./Demo4";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Demo1 />
       <Demo2 />
       <Demo3 />
+      <Demo4 />
     </>
   );
 }
---
+++ src/Demo4.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { useSprings, animated } from "@react-spring/web";
+import { ParentSize } from "@visx/responsive";
+
+const rectStyle = {
+  x: 0,
+  height: "10vmin",
+  background: "#ff6d6d",
+  marginTop: "5vmin",
+  borderRadius: 8
+};
+
+export default function Demo4() {
+  const [widths, setWidths] = useState(
+    Array(10)
+      .fill()
+      .map((val, ind) => (ind + 1) * 10)
+  );
+  const [springs, api] = useSprings(widths.length, (index) => ({
+    width: `${widths[index]}%`
+  }));
+
+  const handleAnimate = () => {
+    const widthsNew = widths.map((val, ind) => Math.random() * 100);
+    api.start((index) => ({ width: `${widthsNew[index]}%` }));
+    setWidths(widthsNew);
+  };
+
+  const handleReset = () => {
+    const widthsNew = Array(10)
+      .fill()
+      .map((val, ind) => (ind + 1) * 10);
+    api.start((index) => ({ width: `${widthsNew[index]}%` }));
+    setWidths(widthsNew);
+  };
+
+  return (
+    <>
+      <p style={{ fontWeight: "bold" }}>Demo 4</p>
+      <p>
+        Using SVG we can explicitly declare the y positioning of squares instead
+        of stacking div.
+      </p>
+      <button onClick={handleAnimate}>Animate</button>
+      <button onClick={handleReset}>Reset</button>
+      <div style={{ aspectRatio: "16/9", width: "50vw" }}>
+        <ParentSize>
+          {({ height, width }) => (
+            <svg height={height} width={width}>
+              {springs.map((styles, ind) => (
+                <animated.rect
+                  x={0}
+                  y={(height / 10) * ind + 0.01 * height}
+                  key={ind}
+                  width={styles.width}
+                  height={(height / 10) * 0.9}
+                  fill="#ff6d6d"
+                />
+              ))}
+            </svg>
+          )}
+        </ParentSize>
+      </div>
+    </>
+  );
+}
